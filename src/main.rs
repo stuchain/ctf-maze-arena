22db3bd0d499533cac0c047c5945d75641cd62c2
Stelios Vasileiou <stelios.vasiliou@icloud.com>
2026-02-03 13:34:00 +0200

    chore: add axum, tokio, serde, tracing deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("ok");
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "info".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    tracing::info!("server starting");
 }
